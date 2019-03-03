ae2fdeed4708d6e315c010a5f642248c47b6c459
Caleb <calebbelcourt@hotmail.com>
2019-03-02 18:02:44 -0800

    Location, Maps and Circles
    Implemented location tracking on a google map with visible circles outlining geofences.

---
+++ app/src/main/java/com/example/scavengerhunt/Constants.java
@@ -0,0 +1,20 @@
+package com.example.scavengerhunt;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+public class Constants {
+
+    public static final int GEOFENCE_EXPIRATION_IN_MILLISECONDS = 10000;
+    public static final float GEOFENCE_RADIUS_IN_METERS = 20;
+
+    public static final HashMap<String, LatLng> LANDMARKS = new     HashMap<String, LatLng>();
+    static {
+        LANDMARKS.put("North Shore", new LatLng(50.704766, -120.3735887));
+
+        LANDMARKS.put("OM North", new LatLng(50.671691, -120.362967));
+
+        LANDMARKS.put("OM South", new LatLng(50.670742, -120.362959));
+    }
+}
---
+++ app/src/main/java/com/example/scavengerhunt/MapsActivity.java
@@ -0,0 +1,281 @@
+package com.example.scavengerhunt;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 102;
+    private GoogleMap mMap;
+    private GeofencingClient geofencingClient;
+    private ArrayList geofenceList;
+    private PendingIntent geofencePendingIntent;
+    private LocationCallback mLocationCallback = new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for(Location location:locationResult.getLocations()){
+                if(location != null){
+                    Log.d("MyLocation", "("+location.getLongitude()+","+location.getLatitude()+")");
+                    //mTextView.setText("( "+location.getLongitude()+" , "+location.getLatitude()+" )");
+                    //Toast.makeText(MapsActivity.this, " ( "+location.getLatitude()+" , "+location.getLongitude()+" ) ", Toast.LENGTH_SHORT).show();
+                    LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+                    Marker m = mMap.addMarker(new MarkerOptions().position(loc).title("Me"));
+                    animateMarker(m, loc, true);
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        createLocationRequest();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        geofencingClient = LocationServices.getGeofencingClient(this);
+        geofenceList = new ArrayList<Geofence>();
+
+        for (Map.Entry<String, LatLng> entry : Constants.LANDMARKS.entrySet()) {
+            geofenceList.add(new Geofence.Builder().setRequestId(entry.getKey())
+
+                    .setCircularRegion(
+                            entry.getValue().latitude,
+                            entry.getValue().longitude,
+                            Constants.GEOFENCE_RADIUS_IN_METERS
+                    )
+                    .setExpirationDuration(-1)
+                    .setLoiteringDelay(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT |
+                            Geofence.GEOFENCE_TRANSITION_DWELL)
+                    .build());
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Geofences added
+                        Toast.makeText(MapsActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to add geofences
+                        Toast.makeText(MapsActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
+/*
+        geofencingClient.removeGeofences(getGeofencePendingIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Geofences removed
+                        Toast.makeText(MapsActivity.this, "Geofence Removed", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to remove geofences
+                        Toast.makeText(MapsActivity.this, "Removal Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                */
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng loc = new LatLng(50.704767, -120.3735888);
+
+        mMap = googleMap;
+        mMap.setMinZoomPreference(18);
+        mMap.setMaxZoomPreference(20);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+
+        for (Map.Entry<String, LatLng> entry : Constants.LANDMARKS.entrySet()){
+            LatLng fence = new LatLng(entry.getValue().latitude, entry.getValue().longitude);
+            CircleOptions circleOptions = new CircleOptions()
+                    .strokeColor(0x5557c4ff)
+                    .fillColor(0x5557c4ff)
+                    .center(fence)
+                    .radius(Constants.GEOFENCE_RADIUS_IN_METERS);
+            Circle circle = mMap.addCircle(circleOptions);
+        }
+    }
+
+    public void animateMarker(final Marker marker, final LatLng toPosition,
+                              final boolean hideMarker) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        Projection proj = mMap.getProjection();
+        Point startPoint = proj.toScreenLocation(marker.getPosition());
+        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+        final long duration = 500;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+                double lng = t * toPosition.longitude + (1 - t)
+                        * startLatLng.longitude;
+                double lat = t * toPosition.latitude + (1 - t)
+                        * startLatLng.latitude;
+                marker.setPosition(new LatLng(lat, lng));
+
+                if (t < 1.0) {
+                    handler.postDelayed(this, 16);
+                } else {
+                    if (hideMarker) {
+                        marker.setVisible(false);
+                    } else {
+                        marker.setVisible(true);
+                    }
+                }
+            }
+        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(hasPermission() == true) {
+            startLocationUpdate();
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdate();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdate(){
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+    }
+
+    private void stopLocationUpdate(){
+        if(mLocationCallback != null)
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+    protected void createLocationRequest() {
+
+        mLocationRequest = LocationRequest.create();
+        //mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(500);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private boolean hasPermission(){
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdate();
+
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(geofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        geofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
+    }
+}
